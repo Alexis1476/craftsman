7c263a1d709e43e31d7b8321bbc97abd9d3483f3
Alexis1476 <halexisr20@hotmail.com>
2022-11-03 13:58:31 +0100

    Attributes added in visitors migration

--- database/migrations/2022_11_03_124013_create_activities_table.php
+++ database/migrations/2022_11_03_124013_create_activities_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->tinyText('why');
+            $table->tinyInteger('points');
+            $table->string('laboratory', 10);
             $table->timestamps();
         });
     }
