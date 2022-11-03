3a6bf733f8c92b5af7685edbef16505219b95b64
Alexis1476 <halexisr20@hotmail.com>
2022-11-03 13:46:06 +0100

    Creation of all models with their controllers / factories / migrations / seeds

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visitor;
+use App\Http\Requests\StoreVisitorRequest;
+use App\Http\Requests\UpdateVisitorRequest;
+
+class VisitorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVisitorRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVisitorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVisitorRequest  $request
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVisitorRequest $request, Visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visitor $visitor)
+    {
+        //
+    }
+}
