aa61624a48d742e22edd77a059dd9c4a25e9c5ca
Alexis1476 <halexisr20@hotmail.com>
2022-11-03 13:59:08 +0100

    Attributes added in visitors migration

--- database/migrations/2022_11_03_123705_create_visitors_table.php
+++ database/migrations/2022_11_03_123705_create_visitors_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('visitors', function (Blueprint $table) {
             $table->id();
+            $table->string('anonymeID', 4);
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('phoneNumber', 15);
+            $table->string('email');
             $table->timestamps();
         });
     }
