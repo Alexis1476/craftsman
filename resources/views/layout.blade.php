6d2e7e92ce90de67d1fb453c19d26d02aa9cd1b5
Alexis1476 <halexisr20@hotmail.com>
2022-11-03 14:22:38 +0100

    Creation basic login form

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="bg-red-700">
-      @yield('content')
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<header>
+    <div></div>
+    <a href="#">Accueil</a>
+</header>
+<main>
+    @yield('content')
+</main>
+<footer>
+
+</footer>
+</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
 
 @section('content')
-<h1>ETML Portes Ouvertes</h1>
+    <h1>ETML Portes Ouvertes</h1>
+    <form action="#">
+        <label for="login">Login</label>
+        <input id="login" type="text">
+        <label for="password">Password</label>
+        <input type="password" id="password">
+        <button type="submit">Se connecter</button>
+    </form>
 @endsection
