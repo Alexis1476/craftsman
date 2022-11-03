cc3ff51bf0b974ce7f50117ce7633e40b2f79aa0
Alexis1476 <halexisr20@hotmail.com>
2022-11-03 13:58:53 +0100

    Attributes added in categories migration

--- database/migrations/2022_11_03_123559_create_categories_table.php
+++ database/migrations/2022_11_03_123559_create_categories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->text('description');
+            $table->string('image');
             $table->timestamps();
         });
     }
