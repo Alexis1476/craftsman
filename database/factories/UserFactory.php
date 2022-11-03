de6c99c0a3912b030cf6ac4a0ba835f871d552d4
Alexis1476 <halexisr20@hotmail.com>
2022-11-03 14:16:46 +0100

    User factory modified and default users added in DatabaseSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,23 +18,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'login' => fake()->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'admin' => '0',
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Student account
+        User::factory()->create([
+            'login' => 'apprINF',
+            'password' => Hash::make('$ETML-PO2022'),
+            'admin' => 0
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Teacher account
+        User::factory()->create([
+            'login' => 'profINF',
+            'password' => Hash::make('$ETML-PO2022'),
+            'admin' => 1
+        ]);
     }
 }
