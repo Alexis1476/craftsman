85cc39102ada3f53fa1dd822fc365121a2d4dee5
Alexis1476 <halexisr20@hotmail.com>
2022-11-07 16:02:00 +0100

    Protected fillable defined

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Visitor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'phoneNumber',
+        'email'
+    ];
 }
