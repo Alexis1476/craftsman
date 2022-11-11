9aaa3f39ec99a2de023bbf149d2c38b6236d2edc
Alexis1476 <halexisr20@hotmail.com>
2022-11-11 23:55:14 +0100

    Migration create pivot table between visitor and activity

---
+++ database/migrations/2022_11_11_225344_create_visitor_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitor_activities', function (Blueprint $table) {
+            $table->integer('visitor_id');
+            $table->integer('activity_id');
+            $table->boolean('finished');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitor_activities');
+    }
+};
