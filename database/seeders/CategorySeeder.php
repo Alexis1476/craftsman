d566079e9b809bfb34ad737c43aa0bb2d7e45ad2
Alexis1476 <halexisr20@hotmail.com>
2022-11-11 19:30:50 +0100

    Seeder to categories and users

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,31 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Challenge
+        Category::factory()->createMany([[
+            'name' => 'Challenge',
+            'description' => '',
+            'image' => ''
+        ], [
+            'name' => 'Culture',
+            'description' => '',
+            'image' => ''
+        ], [
+            'name' => 'Proj-inno',
+            'description' => '',
+            'image' => ''
+        ], [
+            'name' => 'Web-db',
+            'description' => '',
+            'image' => ''
+        ], [
+            'name' => 'Prog-devops',
+            'description' => '',
+            'image' => ''
+        ], [
+            'name' => 'Res-sys',
+            'description' => '',
+            'image' => ''
+        ]]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -16,18 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Student account
-        User::factory()->create([
-            'login' => 'apprINF',
-            'password' => Hash::make('$ETML-PO2022'),
-            'admin' => 0
-        ]);
-
-        // Teacher account
-        User::factory()->create([
-            'login' => 'profINF',
-            'password' => Hash::make('$ETML-PO2022'),
-            'admin' => 1
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
