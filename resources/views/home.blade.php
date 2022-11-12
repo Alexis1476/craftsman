bc7d4ff0dd378e5b3bff58da68bc51d74cd8cd59
Alexis1476 <halexisr20@hotmail.com>
2022-11-12 13:43:34 +0100

    Forms to test login system

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,15 @@
     {{--Form user login--}}
     <form action="{{route('login')}}" method="post">
         {{csrf_field()}}
-        <label for="login">Login</label>
-        <input name="login" id="login" type="text">
-        @if($errors->has('login'))
-            <p>{{$errors->first('login')}}</p>
-        @endif
+        <label for="email">Email</label>
+        <input name="email" id="email" type="email">
         <label for="password">Password</label>
         <input name="password" type="password" id="password">
         @if($errors->has('password'))
             <p>{{$errors->first('password')}}</p>
         @endif
         <button type="submit">Se connecter</button>
-    </form>
+    </form><br><br>
     {{--Form to sign up--}}
     <h2>S'enregistrer</h2>
     <form action="{{route('signUp')}}" method="post">
@@ -41,6 +38,21 @@
         @if($errors->has('email'))
             <p>{{$errors->first('email')}}</p>
         @endif
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password">
         <button type="submit">S'enregistrer</button>
+    </form><br><br>
+    <h1>LOGIN ADMIN</h1>
+    {{--Form user login--}}
+    <form action="{{route('admin.login')}}" method="post">
+        {{csrf_field()}}
+        <label for="login">Login</label>
+        <input name="login" id="login" type="text">
+        <label for="password">Password</label>
+        <input name="password" type="password" id="password">
+        @if($errors->has('password'))
+            <p>{{$errors->first('password')}}</p>
+        @endif
+        <button type="submit">Se connecter</button>
     </form>
 @endsection
