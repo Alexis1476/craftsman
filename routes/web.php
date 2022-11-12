26e4fa5d5a84eb2c2b808b4f98c6cd3dca8c0c22
Alexis1476 <halexisr20@hotmail.com>
2022-11-12 18:06:46 +0100

    Route test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('newActivities', [VisitorController::class, 'newActivities'])->name('test');
+
 Route::get('/', function () {
     return view('home');
 })->name('home');
