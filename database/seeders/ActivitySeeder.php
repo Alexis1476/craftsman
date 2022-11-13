eb9d17190e3064d10de1f24676b0f6dcd23a91b3
Alexis1476 <halexisr20@hotmail.com>
2022-11-13 19:57:20 +0100

    Format data in activity seeder

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -21,7 +21,7 @@ class ActivitySeeder extends Seeder
 
         Activity::factory()->createMany([[
             'name' => 'Lean Talk',
-            'description' => 'Participez à un des workshops proprosés sur differents thèmes',
+            'description' => 'Participez à un des workshops proprosés sur differents thèmes.',
             'why' => 'C’est un espace d’échange sur différentes sujets où les élèves peuvent
                         partager leur connaissance et expériences.',
             'points' => 8,
@@ -31,15 +31,15 @@ class ActivitySeeder extends Seeder
             'name' => 'Positif Feedback',
             'description' => 'Donnez un feedback positif sur un projet, une equipe, un élève, ou un
                                 autre sujet durant votre visite.',
-            'why' => 'C\'est un outil extrêmement important pour aider à l\’amélioration de
-                        chacun.\n Soyez spécifique, essayez de faire un lien positif avec un comportement
+            'why' => 'C\'est un outil extrêmement important pour aider à l’amélioration de
+                        chacun.<br> Soyez spécifique, essayez de faire un lien positif avec un comportement
                         et un résultat.',
             'points' => 3,
             'laboratory' => 'INF',
             'category_id' => $culture->id
         ], [
             'name' => 'Conference',
-            'description' => 'Suivez la conférence de la section informatique.\n
+            'description' => 'Suivez la conférence de la section informatique.<br>
                                 Les horaires:',
             'why' => 'Elle permet de comprendre le cursus du métier, répondre à des
                         questions et avoir une idée plus claire des options que s\'offrent à vous.',
@@ -54,8 +54,8 @@ class ActivitySeeder extends Seeder
         Activity::factory()->createMany([[
             'name' => 'Foreign Language',
             'description' => 'Si vous parlez une autre langue que le français, trouvez un élève qui
-                                parle la même langue, et discutez avec lui sur un sujet de l\'informatique',
-            'why' => 'Etablir des liens / amitiés interculturelles.\n Donner un coup de boost à notre cerveau',
+                                parle la même langue, et discutez avec lui sur un sujet de l\'informatique.',
+            'why' => 'Etablir des liens / amitiés interculturelles.<br>Donner un coup de boost à notre cerveau.',
             'points' => 3,
             'laboratory' => 'INF',
             'category_id' => $challenge->id
@@ -63,7 +63,7 @@ class ActivitySeeder extends Seeder
             'name' => 'Prendre un selfie',
             'description' => 'Prenez une photo originale sur un projet, avec des élèves ou les profs,
                                 soyez créatif! Publiez votre photo sur….',
-            'why' => 'Pour garder un souvenir de votre passage au Portes Ouvertes',
+            'why' => 'Pour garder un souvenir de votre passage au Portes Ouvertes.',
             'points' => 5,
             'laboratory' => 'INF',
             'category_id' => $challenge->id
@@ -84,7 +84,7 @@ class ActivitySeeder extends Seeder
         Activity::factory()->createMany([[
             'name' => 'Product Review',
             'description' => 'Visitez un labo, posez une question à un des élèves. Essayez de
-                                challenger sa réponse, approfondissez le sujet, donnez votre opinion',
+                                challenger sa réponse, approfondissez le sujet, donnez votre opinion.',
             'why' => 'N\'ayez jamais peur de poser des questions. Donnez votre avis!',
             'points' => 7,
             'laboratory' => 'INF',
@@ -94,12 +94,13 @@ class ActivitySeeder extends Seeder
             'description' => 'Apprenez à utiliser GiTHub pour comparer des fichiers, faire un pull
                                 request, merge, etc..',
             'why' => 'Les systèmes de contrôle de version vous permettent de :
-                        • Comparer des fichiers
-                        • Identifier les différences
-                        • Fusionnez les modifications si nécessaire avant de valider un code
-                        • Gardez une trace des versions d\'application en étant capable
+                        <ul>
+                        <li>Comparer des fichiers.</li>
+                        <li>Identifier les différences.</li>
+                        <li>Fusionnez les modifications si nécessaire avant de valider un code.</li>
+                        <li>Gardez une trace des versions d\'application en étant capable.</li>
                         d\'identifier quelle version est actuellement en développement, QA et
-                        production',
+                        production.</li></ul>',
             'points' => 5,
             'laboratory' => 'INF',
             'category_id' => $projinno->id
@@ -113,16 +114,16 @@ class ActivitySeeder extends Seeder
             'category_id' => $projinno->id
         ], [
             'name' => 'Agilite - User Story',
-            'description' => 'Avec l\'aide d\'un élève, apprenez à écrire une User Story',
-            'why' => 'L\'importance de comprendre et bien définir les besoins du client',
+            'description' => 'Avec l\'aide d\'un élève, apprenez à écrire une User Story.',
+            'why' => 'L\'importance de comprendre et bien définir les besoins du client.',
             'points' => 5,
             'laboratory' => 'INF',
             'category_id' => $projinno->id
         ], [
             'name' => 'Serious Game',
             'description' => 'Participez à un jeu sur l\'agilité et décrouvrez cette methode de gestion
-                                de projet',
-            'why' => 'A définir selon le jeu choisi',
+                                de projet.',
+            'why' => 'A définir selon le jeu choisi.',
             'points' => 10,
             'laboratory' => 'INF',
             'category_id' => $projinno->id
@@ -143,7 +144,7 @@ class ActivitySeeder extends Seeder
             'name' => 'Clean Code',
             'description' => 'Avec l\'aide d\'un élève, écrivez une partie de code et testez votre
                                 fonctionnalité.',
-            'why' => 'Un code propre est un code facile à comprendre et facile à modifier.\n
+            'why' => 'Un code propre est un code facile à comprendre et facile à modifier.<br>
                         Facile à comprendre signifie que le code est facile à lire, qu\'il s\'agisse
                         d’un lecteur est l\'auteur original du code ou quelqu\'un d\'autre.',
             'points' => 5,
