1b8e21c8ad40a1b3c94f6628313234b0aead5afc
Alexis1476 <halexisr20@hotmail.com>
2022-11-13 19:58:16 +0100

    Format activities'description to add HTML elements

--- resources/views/visitor/activities.blade.php
+++ resources/views/visitor/activities.blade.php
@@ -6,9 +6,9 @@
     @foreach($activities as $activity)
         <h2>{{$activity->name}}</h2>
         <h4>Description :</h4>
-        <p>{{$activity->description}}</p>
+        <p>{!!$activity->description!!}</p>
         <h4>Pourquoi :</h4>
-        <p>{{$activity->why}}</p>
+        <p>{!!$activity->why!!}</p>
         <span><strong>Points : </strong>{{$activity->points}}</span>
         <p>Laboratoire : {{$activity->laboratory}}</p>
     @endforeach
