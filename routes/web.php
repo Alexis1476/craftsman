063be0521d416fc372c0731511b5e64e28f6f38a
Alexis1476 <halexisr20@hotmail.com>
2022-11-14 15:13:06 +0100

    Route test deleted

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('newActivities', [VisitorController::class, 'newActivities'])->name('test');
-
 Route::get('/', function () {
     return view('home');
 })->name('home');
