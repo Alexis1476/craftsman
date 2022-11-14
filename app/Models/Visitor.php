6ef316235844f6f3a4b24019258829c4fde5f2d3
Alexis1476 <halexisr20@hotmail.com>
2022-11-14 15:13:28 +0100

    NewActivities fonction now in visitorModel

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -29,6 +29,41 @@ class Visitor extends Authenticatable
         'remember_token',
     ];
 
+    public function newActivities()
+    {
+        define('NB_ACTIVITIES', 5);
+
+        // Si nombre d'activités finies n'est pas un multiple de 5
+        $finishedActivities = count($this->activities()->where('finished', 1)->get());
+        if ($finishedActivities % NB_ACTIVITIES != 0)
+            return null;
+
+        // Visiteur qui fini l'activité
+        $visitorActivities = $this->activities->pluck('id'); // Pluck: Get seule les ids
+
+        // Activités que le visiteur n'a pas encore fait
+        $activities = Activity::whereNotIn('id', $visitorActivities)->select()->get()->pluck('id')->toArray();
+
+        // Si le visiteur a déjà fait toutes les activités
+        if (!$activities)
+            return null;
+
+        // Nombre d'activités maximales
+        $nbMaxNewActivities = NB_ACTIVITIES;
+        if (count($activities) < NB_ACTIVITIES)
+            $nbMaxNewActivities = count($activities);
+
+        // Selection de 5 activités random
+        $keyActivities = array_rand($activities, $nbMaxNewActivities);
+
+        // Enregistrement dans la base de données
+        foreach ($keyActivities as $key) {
+            $this->activities()->attach($activities[$key]);
+        }
+
+        return $this->activities;
+    }
+
     public function activities()
     {
         return $this->belongsToMany(Activity::class, 'visitor_activities')->withTimestamps();
