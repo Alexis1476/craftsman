b44e4a59df7ba305ccb19391c5e79bf9d11d4fb8
Alexis1476 <halexisr20@hotmail.com>
2022-11-16 20:09:32 +0100

    Routes created to logout visitor and admin + to show login view

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,20 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('activities', [ActivityController::class, 'index'])->name('activities');
+Route::get('login', function () {
+    return view('login');
+})->name('login');
 
 /* Routes du visiteur*/
 Route::post('login', [VisitorController::class, 'login'])->name('login');
 Route::post('signUp', [VisitorController::class, 'create'])->name('signUp');
+Route::post('logout', [VisitorController::class, 'logout'])->name('logout');
 
 Route::get('activities/{id}', [VisitorController::class, 'show'])->name('visitor.activities');
 
 /*Routes des admins*/
 Route::post('admin/login', [UserController::class, 'login'])->name('admin.login');
+Route::get('admin/logout', [UserController::class, 'logout'])->name('admin.logout');
 
 /*Generer les migrations lors du deploiement*/
 Route::get('migrate', function () {
