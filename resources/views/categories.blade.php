7e99eceff285c0dae849d6d8129343529b129da5
Alexis1476 <halexisr20@hotmail.com>
2022-11-18 19:39:54 +0100

    Categories images displayed and categories description

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,6 +6,8 @@
 
 @foreach($categories as $category)
     <h2>{{$category->name}}</h2>
+    <img src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
+    <p>{{$category->description}}</p>
 @endforeach
 
 @endsection
