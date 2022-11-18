255e30fa9f010577701a0152d44827024cc917ad
Alexis1476 <halexisr20@hotmail.com>
2022-11-18 20:02:45 +0100

    Route to validate an activity

--- routes/web.php
+++ routes/web.php
@@ -24,25 +24,26 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('categories', [CategoryController::class, 'index'])->name('categories');
-Route::get('categories/{name}', [CategoryController::class, ''])->name('category');
+//Route::get('categories/{name}', [CategoryController::class, ''])->name('category');
 Route::get('activities', [ActivityController::class, 'index'])->name('activities');
-Route::get('activities/{name}', [ActivityController::class, ''])->name('activity');
+//Route::get('activities/{name}', [ActivityController::class, ''])->name('activity');
 
-Route::get('login',  function () {
+Route::get('login', function () {
     return view('login');
 })->name('login');
 
 /* Routes du visiteur*/
 Route::post('user/login', [UserController::class, 'login'])->name('user.login');
-Route::post('user/signup', [UserController::class, 'create'])->name('user.signUp');
+Route::post('user/signUp', [UserController::class, 'create'])->name('user.signUp');
 Route::get('user/logout', [UserController::class, 'logout'])->name('user.logout');
 Route::get('user/activities', [UserController::class, 'show'])->name('user.activities');
 
 /* Routes des admins*/
 Route::post('admin/login', [AdminController::class, 'login'])->name('admin.login');
 Route::get('admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
-Route::get('admin/modify', [AdminController::class, ''])->name('admin.modify');
-Route::get('users/{id}', [AdminController::class, ''])->name('admin.finduser');
+//Route::get('admin/modify', [AdminController::class, ''])->name('admin.modify');
+Route::get('user/{id}', [AdminController::class, 'showUser'])->name('admin.showUser');
+Route::get('validateActivity/{user}/{activity}', [AdminController::class, 'validateActivity'])->name('admin.validateActivity');
 Route::get('scores', [ScoreController::class, 'show'])->name('scores');
 
 /* Generer les migrations lors du deploiement*/
