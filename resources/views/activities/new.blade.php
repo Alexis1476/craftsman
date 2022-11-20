be1bbdd0855eb87ec62de2518a653074d8f8b44d
Alexis1476 <halexisr20@hotmail.com>
2022-11-20 17:46:44 +0100

    Display errors for textareas

--- resources/views/activities/new.blade.php
+++ resources/views/activities/new.blade.php
@@ -15,8 +15,14 @@
         @include('components.form-item', ['id' => 'name', 'label' => 'Nom', 'type' => 'text'])
         <label for="description">Description</label>
         <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        @if($errors->has('description'))
+            <p>{{$errors->first('description')}}</p>
+        @endif
         <label for="why">Pourquoi</label>
         <textarea name="why" id="why" cols="30" rows="10"></textarea>
+        @if($errors->has('why'))
+            <p>{{$errors->first('why')}}</p>
+        @endif
         @include('components.form-item', ['id' => 'points', 'label' => 'Points', 'type' => 'number'])
         @include('components.form-item', ['id' => 'laboratory', 'label' => 'Laboratoire', 'type' => 'text'])
         <button type="submit">Ajouter</button>
