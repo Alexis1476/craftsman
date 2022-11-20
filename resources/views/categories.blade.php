084d5e0fedea4b8c361b491771d0afc2526c40e2
Alexis1476 <halexisr20@hotmail.com>
2022-11-20 16:42:00 +0100

    Component created to category card

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,11 +3,7 @@
 @section('title', 'Categories')
 
 @section('content')
-
-@foreach($categories as $category)
-    <h2>{{$category->name}}</h2>
-    <img src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
-    <p>{{$category->description}}</p>
-@endforeach
-
+    @foreach($categories as $category)
+        @include('components.category-card', ['category' => $category])
+    @endforeach
 @endsection
---
+++ resources/views/components/category-card.blade.php
@@ -0,0 +1,3 @@
+<h2>{{$category->name}}</h2>
+<img src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
+<p>{{$category->description}}</p>
