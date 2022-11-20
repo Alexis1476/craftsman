1291ac06325aed69616b46947b085287213d601c
Alexis1476 <halexisr20@hotmail.com>
2022-11-20 17:18:15 +0100

    Get old value in the forms

--- resources/views/components/form-item.blade.php
+++ resources/views/components/form-item.blade.php
@@ -1,5 +1,5 @@
 <label for="{{$id}}">{{$label}}</label>
-<input name="{{$id}}" id="{{$id}}" type="{{$type}}">
+<input name="{{$id}}" id="{{$id}}" type="{{$type}}" value="{{old($id)}}">
 @if($errors->has($id))
     <p>{{$errors->first($id)}}</p>
 @endif
