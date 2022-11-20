b3d187c543b331d429e153e485272ad3f83beb20
Alexis1476 <halexisr20@hotmail.com>
2022-11-20 17:36:27 +0100

    Routes to add new activity

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,8 @@ Route::name('admin.')->group(function () {
             Route::get('user/{id}', 'showUser')->name('showUser');
             Route::get('validateActivity/{user}/{activity}', 'validateActivity')->name('validateActivity');
             Route::get('admin/modify', 'modify')->name('modify');
-            Route::get('admin/addActivity', 'addActivity')->name('addActivity');
+            Route::get('admin/addActivity', 'formAddActivity')->name('formAddActivity');
+            Route::post('admin/addActivity', 'addActivity')->name('addActivity');
             Route::get('scores', [ScoreController::class, 'show'])->name('scores');
         });
     });
