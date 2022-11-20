33e9fa82526a2b875463e814f92c8611c85005bb
Alexis1476 <halexisr20@hotmail.com>
2022-11-20 17:02:10 +0100

    Two forms to user and admin login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,23 +4,12 @@
 
 @section('content')
     {{--Form user login--}}
+    <h1>LOGIN USER</h1>
     <form action="{{route('user.login')}}" method="post">
         {{csrf_field()}}
         @include('components.form-item',['id' => 'email', 'label'=> 'Email', 'type' => 'email'])
         @include('components.form-item',['id' => 'password', 'label'=> 'Mot de passe', 'type' => 'password'])
         <button type="submit">Se connecter</button>
-    </form><br><br>
-    {{--Form to sign up--}}
-    <h2>S'enregistrer</h2>
-    <form action="{{route('user.signUp')}}" method="post">
-        {{csrf_field()}}
-        @include('components.form-item',['id' => 'firstName', 'label'=> 'Prénom', 'type' => 'text'])
-        @include('components.form-item',['id' => 'lastName', 'label'=> 'Nom de famille', 'type' => 'text'])
-        @include('components.form-item',['id' => 'phoneNumber', 'label'=> 'Numéro de téléphone', 'type' => 'text'])
-        @include('components.form-item',['id' => 'email', 'label'=> 'Email', 'type' => 'email'])
-        @include('components.form-item',['id' => 'password', 'label'=> 'Mot de passe', 'type' => 'password'])
-        @include('components.form-item',['id' => 'password_confirmation', 'label'=> 'Confirmation du mot de passe', 'type' => 'password'])
-        <button type="submit">S'enregistrer</button>
     </form><br><br>
     <h1>LOGIN ADMIN</h1>
     {{--Form user login--}}
