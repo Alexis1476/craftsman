c2b763f8dadceca0acf920520c4f706988ad6402
Alexis1476 <halexisr20@hotmail.com>
2022-11-20 17:08:59 +0100

    Condition to show signup form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,16 +12,19 @@
 @section('content')
     <h2>Craftman Challenge</h2>
     {{--Form to sign up--}}
-    <h2>S'enregistrer</h2>
-    <form action="{{route('user.signUp')}}" method="post">
-        {{csrf_field()}}
-        @include('components.form-item',['id' => 'firstName', 'label'=> 'Prénom', 'type' => 'text'])
-        @include('components.form-item',['id' => 'lastName', 'label'=> 'Nom de famille', 'type' => 'text'])
-        @include('components.form-item',['id' => 'phoneNumber', 'label'=> 'Numéro de téléphone', 'type' => 'text'])
-        @include('components.form-item',['id' => 'email', 'label'=> 'Email', 'type' => 'email'])
-        @include('components.form-item',['id' => 'password', 'label'=> 'Mot de passe', 'type' => 'password'])
-        @include('components.form-item',['id' => 'password_confirmation', 'label'=> 'Confirmation du mot de passe', 'type' => 'password'])
-        <button type="submit">S'enregistrer</button>
-    </form>
-
+    @auth('webadmin')
+    @elseauth('web')
+    @else
+        <h2>S'enregistrer</h2>
+        <form action="{{route('user.signUp')}}" method="post">
+            {{csrf_field()}}
+            @include('components.form-item',['id' => 'firstName', 'label'=> 'Prénom', 'type' => 'text'])
+            @include('components.form-item',['id' => 'lastName', 'label'=> 'Nom de famille', 'type' => 'text'])
+            @include('components.form-item',['id' => 'phoneNumber', 'label'=> 'Numéro de téléphone', 'type' => 'text'])
+            @include('components.form-item',['id' => 'email', 'label'=> 'Email', 'type' => 'email'])
+            @include('components.form-item',['id' => 'password', 'label'=> 'Mot de passe', 'type' => 'password'])
+            @include('components.form-item',['id' => 'password_confirmation', 'label'=> 'Confirmation du mot de passe', 'type' => 'password'])
+            <button type="submit">S'enregistrer</button>
+        </form>
+    @endguest
 @endsection
