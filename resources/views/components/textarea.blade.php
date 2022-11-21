757cef6f45c8bbe50fea9815bd771f8ec445e99c
Alexis1476 <halexisr20@hotmail.com>
2022-11-21 15:19:30 +0100

    Parameter description renamed in label

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,4 +1,4 @@
-<label for="{{$id}}">{{$description}}</label>
+<label for="{{$id}}">{{$label}}</label>
 <textarea name="{{$id}}" id="{{$id}}" cols="30" rows="10">{{isset($value) ? $value : old($id)}}</textarea>
 @if($errors->has($id))
     <p>{{$errors->first($id)}}</p>
