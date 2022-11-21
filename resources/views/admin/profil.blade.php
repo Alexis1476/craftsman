25769991e3b242338e4adc02a79f06be54ac96b4
Alexis1476 <halexisr20@hotmail.com>
2022-11-21 16:18:31 +0100

    Profil view to change password

--- resources/views/admin/profil.blade.php
+++ resources/views/admin/profil.blade.php
@@ -3,5 +3,11 @@
 @section('title', 'Profil')
 
 @section('content')
-
+    {{auth('webadmin')->user()->login}}
+    <form action="{{route('admin.update')}}" method="post">
+        {{csrf_field()}}
+        @include('components.form-item', ['id' => 'password', 'label' => 'Mot de passe', 'type' => 'password'])
+        @include('components.form-item', ['id' => 'password_confirmation', 'label' => 'Confirmation du mot de passe', 'type' => 'password'])
+        <button type="submit">Changer</button>
+    </form>
 @endsection
