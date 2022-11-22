159469fb250c7c54d1709c1f2c4b96408cd3ee96
Alexis1476 <halexisr20@hotmail.com>
2022-11-22 19:26:29 +0100

    Tailwind CDN added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
