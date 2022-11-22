5f3b7be23b2eaf7bbb47f77f9c1f37694e4a20a9
Alexis1476 <halexisr20@hotmail.com>
2022-11-22 14:44:36 +0100

    Button to add an activity and to show all activities

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Categories')
 
 @section('content')
+    @auth('webadmin')
+        @if(auth('webadmin')->user()->right === 1)
+            <a href="{{route('admin.formAddActivity')}}">Ajouter</a>
+        @endif
+    @endauth
+    @include('components.nav-item', ['route' => route('activities'), 'text' => 'Voir toutes'])
     @foreach($categories as $category)
         @include('components.category-card', ['category' => $category])
     @endforeach
