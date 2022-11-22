181dd4d7b53e61e6447157bd1fdffcb278c153a2
Alexis1476 <halexisr20@hotmail.com>
2022-11-22 19:57:38 +0100

    Input style copied in textarea component

--- resources/views/components/form-item.blade.php
+++ resources/views/components/form-item.blade.php
@@ -1,7 +1,7 @@
 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="{{$id}}">{{$label}}</label>
     <input
-        class="@error($id) border-red-500 @enderror appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white "
+        class="@error($id) border-red-500 @enderror appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
         name="{{$id}}" id="{{$id}}" type="{{$type}}"
         value="{{isset($value) ? $value : old($id)}}">
     @if($errors->has($id))
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,5 +1,9 @@
-<label for="{{$id}}">{{$label}}</label>
-<textarea name="{{$id}}" id="{{$id}}" cols="30" rows="10">{{isset($value) ? $value : old($id)}}</textarea>
-@if($errors->has($id))
-    <p>{{$errors->first($id)}}</p>
-@endif
+<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="{{$id}}">{{$label}}</label>
+    <textarea
+        class="@error($id) border-red-500 @enderror appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+        name="{{$id}}" id="{{$id}}" cols="30" rows="10">{{isset($value) ? $value : old($id)}}</textarea>
+    @if($errors->has($id))
+        <p class="text-red-500 text-xs italic">{{$errors->first($id)}}</p>
+    @endif
+</div>
