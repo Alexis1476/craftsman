19a0e74893d8f16d2e36c75b23bd52f538a3a1b7
Alexis1476 <halexisr20@hotmail.com>
2022-11-22 19:27:04 +0100

    Style component input-item

--- resources/views/components/form-item.blade.php
+++ resources/views/components/form-item.blade.php
@@ -1,5 +1,10 @@
-<label for="{{$id}}">{{$label}}</label>
-<input name="{{$id}}" id="{{$id}}" type="{{$type}}" value="{{isset($value) ? $value : old($id)}}">
-@if($errors->has($id))
-    <p>{{$errors->first($id)}}</p>
-@endif
+<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="{{$id}}">{{$label}}</label>
+    <input
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white @error($id) border-red-500"
+        @enderror name="{{$id}}" id="{{$id}}" type="{{$type}}"
+        value="{{isset($value) ? $value : old($id)}}">
+    @if($errors->has($id))
+        <p class="text-red-500 text-xs italic">{{$errors->first($id)}}</p>
+    @endif
+</div>
