c45c9603a106610a40cc70fe753104832e3d3c31
Alexis1476 <halexisr20@hotmail.com>
2022-11-22 19:45:51 +0100

    Style form change password

--- resources/views/admin/profil.blade.php
+++ resources/views/admin/profil.blade.php
@@ -4,10 +4,12 @@
 
 @section('content')
     {{auth('webadmin')->user()->login}}
-    <form action="{{route('admin.update')}}" method="post">
+    <form class="w-full max-w-lg" action="{{route('admin.update')}}" method="post">
         {{csrf_field()}}
-        @include('components.form-item', ['id' => 'password', 'label' => 'Mot de passe', 'type' => 'password'])
-        @include('components.form-item', ['id' => 'password_confirmation', 'label' => 'Confirmation du mot de passe', 'type' => 'password'])
-        <button type="submit">Changer</button>
+        <div class="flex flex-wrap -mx-3 mb-2">
+            @include('components.form-item', ['id' => 'password', 'label' => 'Mot de passe', 'type' => 'password'])
+            @include('components.form-item', ['id' => 'password_confirmation', 'label' => 'Confirmation du mot de passe', 'type' => 'password'])
+        </div>
+        <button class="mx-auto bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" type="submit">Changer</button>
     </form>
 @endsection
