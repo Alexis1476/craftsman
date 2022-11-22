9a85147de324719baa3d2e815b633198f7a5fa6a
Alexis1476 <halexisr20@hotmail.com>
2022-11-22 14:44:13 +0100

    Category's description in comments

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,3 +1,3 @@
-<h2>{{$category->name}}</h2>
+<a href="{{route('category',['id'=>$category])}}"><h2>{{$category->name}}</h2></a>
 <img src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
-<p>{{$category->description}}</p>
+{{--<p>{{$category->description}}</p>--}}
