b9b6727d77d69f1debab0bdd391e252d2c88a1e4
Alexis1476 <halexisr20@hotmail.com>
2022-11-25 14:40:05 +0100

    Title modified

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,13 +3,14 @@
 @section('title', 'Categories')
 
 @section('content')
-    <div class="w-full">
+    <h1 class="text-3xl font-bold mb-2">Categories</h1>
+    <div class="w-full my-4 flex justify-center">
         @auth('webadmin')
             @if(auth('webadmin')->user()->right === 1)
-                <a href="{{route('admin.formAddActivity')}}">Ajouter</a>
+                <a class="bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" href="{{route('admin.formAddActivity')}}">Ajouter</a>
             @endif
         @endauth
-        @include('components.nav-item', ['route' => route('activities'), 'text' => 'Voir toutes'])
+            <a class="ml-5 bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" href="{{route('activities')}}">Voir tout</a>
     </div>
     <div class="w-full">
         @foreach($categories as $category)
