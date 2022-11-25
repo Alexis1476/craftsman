fcf9fdb1fd414e4cc9e846656bfa3b709589d092
Alexis1476 <halexisr20@hotmail.com>
2022-11-25 14:10:23 +0100

    Category card design done

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,5 +1,14 @@
-<div>
-    <img src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
-    <a class="font-bold text-xl mb-2" href="{{route('category',['id'=>$category])}}">{{$category->name}}</a>
+<div class="bg-white rounded-[35px] shadow-lg flex mb-4 overflow-hidden">
+    <img width="130" src="{{asset("img/activities/$category->image")}}" alt="Categorie" class="rounded-t-lg">
+    <div class="p-6">
+        <h2 class="font-bold mb-2 text-2xl text-purple-800">{{$category->name}}
+        </h2>
+        <a href="{{route('category',['id'=>$category])}}"
+           class="text-purple-600 hover:text-purple-500 underline text-sm">Voir les activités</a>
+    </div>
 </div>
+{{--<div class="flex flex-col justify-center">
+    <img width="130" src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
+    <a class="font-bold text-xl mb-2" href="{{route('category',['id'=>$category])}}">{{$category->name}}</a>
+</div>--}}
 {{--<p>{{$category->description}}</p>--}}
