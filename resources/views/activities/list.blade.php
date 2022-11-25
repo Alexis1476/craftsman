cdf930135eee4ff93ca2ef820e20c25f612c2cd1
Alexis1476 <halexisr20@hotmail.com>
2022-11-25 15:50:14 +0100

    Table to show activities

--- resources/views/activities/list.blade.php
+++ resources/views/activities/list.blade.php
@@ -4,12 +4,42 @@
 
 @section('content')
     @if(isset($category))
-        <h2>{{$category->name}}</h2>
-        <img src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
+        <h1 class="text-3xl font-bold mb-8">{{$category->name}}</h1>
+        <img class="mb-8" src="{{asset("img/activities/$category->image")}}" alt="{{$category->name}}">
+    @else
+        <h1 class="text-3xl font-bold mb-8">Activités</h1>
     @endif
-    @foreach($activities as $activity)
-        <a href="{{route('activity',['id' => $activity->id])}}">{{$activity->name}}</a>
-        <span><strong>Points : </strong>{{$activity->points}}</span>
-        <p>Laboratoire : {{$activity->laboratory}}</p>
-    @endforeach
+    @if($activities)
+        <table class="text-sm text-left text-gray-500 table-auto max-w-[90%] mx-auto">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+                <th class="p-3">
+                    Nom
+                </th>
+                <th class="p-3">
+                    Laboratoire
+                </th>
+                <th class="p-3">
+                    Points
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($activities as $activity)
+                <tr class="hover:bg-gray-50">
+                    <th class="p-3 font-medium text-gray-900">
+                        <a href="{{route('activity',['id' => $activity->id])}}"
+                           class="font-medium text-blue-600 hover:underline">{{$activity->name}}</a>
+                    </th>
+                    <td class="p-3">
+                        {{$activity->laboratory}}
+                    </td>
+                    <td class="p-3">
+                        {{$activity->points}}
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endif
 @endsection
