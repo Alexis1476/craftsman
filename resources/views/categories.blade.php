529891924105e4c3e9d35a7916e0afbab8375a4a
Alexis1476 <halexisr20@hotmail.com>
2022-11-25 15:08:28 +0100

    Space between childrens added in categories view

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
     <h1 class="text-3xl font-bold mb-8">Categories</h1>
-    <div class="w-full my-4 flex justify-center">
+    <div class="w-full my-4 flex justify-center space-x-4">
         @auth('webadmin')
             @if(auth('webadmin')->user()->right === 1)
                 <a class="bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" href="{{route('admin.formAddActivity')}}">Ajouter</a>
             @endif
         @endauth
-            <a class="ml-5 bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" href="{{route('activities')}}">Voir tout</a>
+            <a class="bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" href="{{route('activities')}}">Voir tout</a>
     </div>
     <div class="w-full">
         @foreach($categories as $category)
