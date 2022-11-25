3041c47561cc4afc5cf3ef17d19c945a5d1c45e4
Alexis1476 <halexisr20@hotmail.com>
2022-11-25 15:07:27 +0100

    Title added to activity view

--- resources/views/activities/activity.blade.php
+++ resources/views/activities/activity.blade.php
@@ -3,13 +3,13 @@
 @section('title', "$activity->name")
 
 @section('content')
+    <h1 class="text-3xl font-bold mb-8">{{$activity->name}}</h1>
     @auth('webadmin')
         {{--Si l'admin est un prof--}}
         @if(auth('webadmin')->user()->right === 1)
             @include('components.activity-form',['route' => route('admin.addActivity'), 'categories' => $categories, 'activity' => $activity])
         @endif
     @else
-        <h1>{{$activity->name}}</h1>
         <p>Description : {!!$activity->description!!}</p>
         <p>Pourquoi : {!!$activity->why!!}</p>
         <p>Points : {{$activity->points}}</p>
