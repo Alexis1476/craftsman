0d264d090aa49cb058142ee9a4f5c14a4253fb65
Alexis1476 <halexisr20@hotmail.com>
2022-11-25 14:27:41 +0100

    Style login forms

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,23 +4,31 @@
 
 @section('content')
     {{--Form user login--}}
-    <h2>Visiteur</h2>
-    <form class="w-full max-w-lg" action="{{route('user.login')}}" method="post">
-        {{csrf_field()}}
-        <div class="flex flex-wrap -mx-3 mb-2">
-            @include('components.form-item',['id' => 'email', 'label'=> 'Email', 'type' => 'email'])
-            @include('components.form-item',['id' => 'password', 'label'=> 'Mot de passe', 'type' => 'password'])
-        </div>
-        <button class="bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" type="submit">Se connecter</button>
-    </form>
-    <h2>Admin</h2>
-    {{--Form user login--}}
-    <form class="w-full max-w-lg" action="{{route('admin.login')}}" method="post">
-        {{csrf_field()}}
-        <div class="flex flex-wrap -mx-3 mb-2">
-            @include('components.form-item',['id' => 'login', 'label'=> 'Nom d\'utilisateur', 'type' => 'text'])
-            @include('components.form-item',['id' => 'password_admin', 'label'=> 'Mot de passe', 'type' => 'password'])
-        </div>
-        <button class="mx-auto bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" type="submit">Se connecter</button>
-    </form>
+    <div class="flex flex-col my-4">
+        <h3 class="text-xl font-semibold text-center">Visiteur</h3>
+        <form class="w-full max-w-lg" action="{{route('user.login')}}" method="post">
+            {{csrf_field()}}
+            <div class="flex flex-wrap -mx-3 mb-2">
+                @include('components.form-item',['id' => 'email', 'label'=> 'Email', 'type' => 'email'])
+                @include('components.form-item',['id' => 'password', 'label'=> 'Mot de passe', 'type' => 'password'])
+            </div>
+            <button class="bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full" type="submit">Se
+                connecter
+            </button>
+        </form>
+    </div>
+    <div class="flex flex-col my-4">
+        <h3 class="text-xl font-semibold text-center">Admin</h3>
+        {{--Form user login--}}
+        <form class="w-full max-w-lg" action="{{route('admin.login')}}" method="post">
+            {{csrf_field()}}
+            <div class="flex flex-wrap -mx-3 mb-2">
+                @include('components.form-item',['id' => 'login', 'label'=> 'Nom d\'utilisateur', 'type' => 'text'])
+                @include('components.form-item',['id' => 'password_admin', 'label'=> 'Mot de passe', 'type' => 'password'])
+            </div>
+            <button class="mx-auto bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full"
+                    type="submit">Se connecter
+            </button>
+        </form>
+    </div>
 @endsection
