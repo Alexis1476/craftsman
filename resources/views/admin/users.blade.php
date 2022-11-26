3a809cc7f04af38278c62697b22a5b1a16f2e30a
Alexis1476 <halexisr20@hotmail.com>
2022-11-26 19:42:58 +0100

    Form added to search visiteur by anonymousID

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     <h1 class="text-3xl font-bold mb-8">Visiteurs</h1>
+    <form class="flex items-center" action="{{route('admin.searchUser')}}" method="post">
+        {{csrf_field()}}
+        @include('components.form-item', ['id' => 'id', 'label' => 'User ID', 'type' => 'text'])
+        <button class="mx-auto bg-[#371877] hover:opacity-75 text-white font-bold py-2 px-4 rounded-full h-fit"
+                type="submit">Chercher
+        </button>
+    </form>
     @forelse($users as $user)
         <a href="{{route('admin.showUser',['id' => $user->anonymousID])}}">{{$user->anonymousID}}</a>
     @empty
