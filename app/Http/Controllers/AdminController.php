1bd4f45991acff6c71bf1e7b645338611ba125ba
Alexis1476 <halexisr20@hotmail.com>
2022-11-26 20:11:46 +0100

    Function in userModel to display score

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,7 +64,7 @@ class AdminController extends Controller
         $user = User::where('anonymousID', \request('id'))->firstOrFail();
         $userActivities = $user->activities()->where('finished', 0)->get();
         $score = $user->score();
-        return View('user.activities', ['user' => $user, 'score' => $score, 'activities' => $userActivities]);
+        return View('user.activities', ['user' => $user, 'activities' => $userActivities]);
     }
 
     //
--- resources/views/user/activities.blade.php
+++ resources/views/user/activities.blade.php
@@ -5,9 +5,7 @@
 @section('content')
     <h1 class="font-bold mb-2 text-3xl text-[#371877]">{{$user->anonymousID}}</h1>
     <p class="italic">{{$user->email}}</p>
-    @if(isset($score))
-        <p><strong>Total de points :</strong> {{$score}}</p>
-    @endif
+    <p><strong>Total de points :</strong> {{$user->score()}}</p>
     <div class="flex flex-wrap gap-5 justify-center my-6">
         @foreach($activities as $activity)
             @include('components.activity-card', ['activity' => $activity, 'imgCategory' => $activity->category->image])
